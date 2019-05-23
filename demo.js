5b637bdd54eb58c6124fc423f149759131eec2e3
zhangyy.dz <zhangyyfangzheng@163.com>
2019-05-23 13:35:41 +0800

    iScroll

--- demo.js
+++ demo.js
@@ -5,17 +5,29 @@
 //     return top <= viewPortHeight + 100
 // }
 
-var bfscrolltop = document.getElementsByTagName('ul')[0].scrollTop;
+// var inputArray = $('input')
+// console.log(inputArray)
+// $('input').bind('focus', function() {
+//     $('input').css('position', 'static');
+//     //或者$('#viewport').height($(window).height()+'px'); 
+//     console.log($(input).css)
+// }).bind('blur', function() {
+//     $('input').css({ 'position': 'fixed', 'bottom': '0' });
+//     //或者$('#viewport').height('auto'); 
+// });
+var bfscrolltop = $('ul')[0].scrollTop;
+
 $("input").focus(function() {
+    console.log(bfscrolltop)
     document.body.scrollTop = document.body.scrollHeight;
     //console.log(document.body.scrollTop);
 }).blur(function() {
     document.body.scrollTop = bfscrolltop;
     //console.log(document.body.scrollTop);
 });
-// if(!!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) || navigator.userAgent.indexOf('micromessenger') > -1){
-//     self.textInput.addEventListener('blur',function(){
-//         window.scrollTo(0,0);
+// if (!!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) || navigator.userAgent.indexOf('micromessenger') > -1) {
+//     self.textInput.addEventListener('blur', function() {
+//         window.scrollTo(0, 0);
 //         console.log('lose point')
 //     })
 // }
